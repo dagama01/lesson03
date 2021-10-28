2c8d0b8ef4ff7aaceef3e0d0a2308c5c1fafeecf
Kanat <abdsattar02@inbox.ru>
2021-10-28 18:26:01 +0600

    Solved tasks 1 and 2

--- src/main/java/kz/gb/kanat/Main.java
+++ src/main/java/kz/gb/kanat/Main.java
@@ -1,5 +1,27 @@
 package kz.gb.kanat;
 
-public class Main {
 
+public class Main {
+    public static void main(String[] args) {
+    fromOneToOneHundred();
+    }
+    public static void replaceZeroOne(){
+        int[] arr = { 1, 0, 1, 0, 0, 1 };
+        for(int i=0; i<arr.length; i++){
+        if(arr[i]==1){
+                arr[i]=0;
+            }
+            else{
+                arr[i]=1;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void fromOneToOneHundred(){
+        int[] arr = new int[100];
+        for (int i=0; i<arr.length; i++){
+            arr[i]+=i;
+            System.out.println(arr[i]+1);
+        }
+    }
 }
