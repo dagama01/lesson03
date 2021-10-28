96b013df757fe8822df800a90aa1d21c0c14cae4
Kanat <abdsattar02@inbox.ru>
2021-10-28 18:35:09 +0600

    Solved task 3

--- src/main/java/kz/gb/kanat/Main.java
+++ src/main/java/kz/gb/kanat/Main.java
@@ -3,7 +3,7 @@ package kz.gb.kanat;
 
 public class Main {
     public static void main(String[] args) {
-    fromOneToOneHundred();
+    toDivideNumbersLessThanSix();
     }
     public static void replaceZeroOne(){
         int[] arr = { 1, 0, 1, 0, 0, 1 };
@@ -22,6 +22,15 @@ public class Main {
         for (int i=0; i<arr.length; i++){
             arr[i]+=i;
             System.out.println(arr[i]+1);
+        }
+    }
+    public static void toDivideNumbersLessThanSix(){
+        int[] arr =  {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i=0; i<arr.length; i++){
+            if (arr[i]<6){
+                arr[i] *= 2;
+            }
+            System.out.print(arr[i] + " ");
         }
     }
 }
